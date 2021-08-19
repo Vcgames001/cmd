28cb9184cded2eaf3542027281ea7c6c4d97c367
Douglas571 <juancarlossocorro571@gmail.com>
2021-08-19 00:41:24 -0400

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b634cfe6c95fc4f6f6640d731b097509'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b99389fa-1872-4ed5-8424-20719f5f84a3'
+};
